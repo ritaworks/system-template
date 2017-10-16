a13f596a0d6902610009cda8bb26a2ffda1075b7
tanpopo <b2094@oic.jp>
2017-10-16 19:33:22 +0900

    npm関係ファイル追加

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var SRC = '';
+
+var path = require('path');
+
+//compaile
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+
+//sass
+var sass = require('gulp-sass');
+var sassglob = require('gulp-sass-glob');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+  gulp.src(path.join(SRC, 'sass/import.scss'))
+    .pipe(plumber())
+    .pipe(sassglob())
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write(path.join(SRC, 'css/')))
+    .pipe(gulp.dest(path.join(SRC, 'css/')));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(path.join(SRC, 'sass/**/*.scss'), ['sass']);
+});
+
+gulp.task('default', ['watch']);
